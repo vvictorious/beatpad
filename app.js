5e7406f48e5d4b586cbb5aa9b8eabfdc207325c6
vvictorious <vvictorious30@gmail.com>
2019-05-28 15:30:18 -0600

    just kidding. now it is working how it is supposed to be

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 console.log('Hello World');
-const key = document.querySelector('.key');
+
 
 window.addEventListener('keydown', function(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	if(!audio) return;
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add('playing');
 });
+
+
+
+function removeClass(){
+
+}
+
 
 
 

24cbc81122cdfb471d7007f481119e83d0f4f2a5
vvictorious <vvictorious30@gmail.com>
2019-05-28 15:50:30 -0600

    got the playing class to toggle and end when transition ends

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ console.log('Hello World');
 
 window.addEventListener('keydown', function(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	const keys = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	if(!audio) return;
 	audio.currentTime = 0;
 	audio.play();
-	key.classList.add('playing');
+	keys.classList.add('playing');
 });
 
 
 
-function removeClass(){
-
+function removeClass(e){
+	this.classList.remove('playing');
 }
 
-
+const keys =document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
 
 

f7d59c11e09f832870880bb0358e2fc4f5b5d247
vvictorious <vvictorious30@gmail.com>
2019-05-28 15:28:00 -0600

    adding border to key on keydown

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 console.log('Hello World');
+const key = document.querySelector('.key');
 
 window.addEventListener('keydown', function(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 	if(!audio) return;
 	audio.currentTime = 0;
 	audio.play();
+	key.classList.add('playing');
 });
+
+
+

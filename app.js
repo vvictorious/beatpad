0b702d5ae336e7622d69d6f952fa7d43567eb044
vvictorious <vvictorious30@gmail.com>
2019-05-28 15:22:30 -0600

    the sounds are now playing

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log('Hello World');
 
 window.addEventListener('keydown', function(e){
-	console.log(e.keyCode);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	if(!audio) return;
+	audio.currentTime = 0;
+	audio.play();
 });

601c4050d0fa223e173083daf97d2cc8523564a8
vvictorious <vvictorious30@gmail.com>
2019-05-30 12:34:31 -0600

    deploying to heroku

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 console.log('Hello World');
 
 
+function removeClass(e){
+	this.classList.remove('playing');
+};
+
+
+
+
 window.addEventListener('keydown', function(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 	const keys = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -12,11 +19,10 @@ window.addEventListener('keydown', function(e){
 
 
 
-function removeClass(e){
-	this.classList.remove('playing');
-}
 
 const keys =document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeClass));
+
+//next make this work on an click event listener
 
 
